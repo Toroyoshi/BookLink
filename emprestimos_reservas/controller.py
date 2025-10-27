21c43abd11e3bc9dd53f45911619f137dacffcd6
Toroyoshi <121224137+Toroyoshi@users.noreply.github.com>
2025-10-27 14:25:06 +0000

    MS empréstimos/reservas

---
+++ emprestimos_reservas/controller.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, request, jsonify
+from  model import Loanoan
+import requests
+
+loan_blueprint = Blueprint('loan', __name__)
+
+loans = {}
+
+def verificar_user(user_id):
+    try:
+        response = requests.get(f'http://localhost:5000/users/{user_id}')
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException as e:
+        print("Erro ao conectar ao serviço de utilizadores", e)
+
+
+@loan_blueprint.route('/<int:loan_id>', methods=['GET'])
+def get_loan(loan_id):
+    loan = loans.get(loan_id)
+    if loan:
+        return jsonify(loan.to_dict())
+    return jsonify({'error': 'loan not found'}), 404
+
+@loan_blueprint.route('/', methods=['POST'])
+def create_loan():
+    loan_data = request.json
+    user_id = loan_data['user_id']
+    
+    # Verifica se o user existe no serviço de utilizadores
+    if not verificar_user(user_id):
+        return jsonify({'error': 'User does not exist'}), 400
+    
+    # Cria o pedido se o user existir
+    loan = loan(loan_id=loan_data['loan_id'], user_id=user_id, product_details=loan_data['product_details'])
+    loans[loan.loan_id] = loan
+    return jsonify(loan.to_dict()), 201
